d365c9773241107e0a258f6a902fd9fda91e219f
lavanyanami01-byte <lavanyanami01@gmail.com>
2025-08-02 11:40:22 +0530

    Time: 8 ms (69.71%), Space: 12.4 MB (51.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+       str1=str(x)
+       rev=str1[::-1]
+       if str1==rev:
+           return True
+       return False
+        
+        
