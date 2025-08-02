44d93b75eac3a25a44df90d7c8385b521ed9603d
lavanyanami01-byte <lavanyanami01@gmail.com>
2025-08-02 12:38:40 +0530

    Time: 143 ms (16.28%), Space: 13.1 MB (55.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+       s=s.lower()
+       rev=""
+       for char in s:
+            if char.isalpha() or char.isdigit():
+                rev=rev+char
+       print(rev)
+       return rev==rev[::-1]
